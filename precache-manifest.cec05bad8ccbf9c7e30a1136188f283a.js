06d4e8a74a92c12a98fbaa4531e8adcbe01e47fc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-10 20:36:46 +0000

    Deploy Zomzog/tic-tac-toe to github.com/Zomzog/tic-tac-toe.git:gh-pages

---
+++ precache-manifest.cec05bad8ccbf9c7e30a1136188f283a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80116ea519fe52adf907",
+    "url": "/tic-tac-toe/static/css/main.f2454ffe.chunk.css"
+  },
+  {
+    "revision": "80116ea519fe52adf907",
+    "url": "/tic-tac-toe/static/js/main.80116ea5.chunk.js"
+  },
+  {
+    "revision": "958d2263acedb09ac06c",
+    "url": "/tic-tac-toe/static/js/1.958d2263.chunk.js"
+  },
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "23a9b249d314bf2a2e62efdbfd45b64d",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
